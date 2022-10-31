d0286936cddddfa10440fc73e861668b7094b7f5
wahyuningrat005 <wahyu09hd@gmail.com>
2022-10-31 08:26:49 +0800

    memasukkan materi number

---
+++ lib/number.dart
@@ -0,0 +1,15 @@
+/* tipe data number
+terdapat dua jenis tipe data number,Int dan Double
+doubel adalah tipe data bilangan desimal
+Integer adalah tipe data bilang bulat
+
+*/
+void main() {
+//contoh tpe data integer
+  int number = 10;
+
+// contoh tipe data double
+  double number1 = 5.0;
+  print(number);
+  print(number1);
+}
