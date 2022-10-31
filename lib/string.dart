4f45ea43e5f1ac83119311b20da868c27e2335fd
wahyuningrat005 <wahyu09hd@gmail.com>
2022-10-31 09:10:46 +0800

    menambah file string

---
+++ lib/string.dart
@@ -0,0 +1,12 @@
+/*
+string merupakan tipe data text atau tuisan
+untuk membuat string,kita bisa menggunakan tanda kutip satu atau kutip dua,
+lalu didalamnya berisi nilai textnya
+disarankan menggunakan kutip satu saja
+*/
+
+void main() {
+  String nama = 'wahyu';
+
+  print(nama);
+}
