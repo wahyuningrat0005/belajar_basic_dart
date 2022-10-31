86ffd931fb42ac139ec0e55feadfa2dd7ee2da94
wahyuningrat005 <wahyu09hd@gmail.com>
2022-10-31 08:35:54 +0800

    menambah kode boolean

---
+++ lib/boolean.dart
@@ -0,0 +1,14 @@
+/*
+tipe data boolean
+boolean adalah tipe data yang hanya memilki dua nilai
+yaitu salah dan benar
+boolean diresepsantikan dengan kata kunci bool
+untuk nilai benar menggunakan kata kunci true
+untuk nilai salah menggunakan kata kunci false
+*/
+void main() {
+  bool mulai = false;
+  print(mulai);
+  mulai = true;
+  print(mulai);
+}
