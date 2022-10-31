cc506e1de40c71ced562bc1aa866a9df68c2ec3d
wahyuningrat005 <wahyu09hd@gmail.com>
2022-10-31 09:57:55 +0800

    menambah file string interpulation

---
+++ lib/string_interpolution.dart
@@ -0,0 +1,14 @@
+/*
+string interpolution
+
+string yang mendukung expression,dimana didalam
+expression kita bisa mengambil data dari variable lain
+
+*/
+void main() {
+  String firstName = 'wahyu';
+  String lastName = 'ningrat';
+  var fullName = '$firstName ${lastName}';
+
+  print(fullName);
+}
